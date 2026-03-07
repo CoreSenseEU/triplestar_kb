06ce8f1ded0fd28893fa4a37d742e247437e7ee5
Marijn de Rijk <derijkmarijn00@gmail.com>
2026-03-07 09:29:45 +0000

    debug logging for query insertion

--- triplestar_kb/triplestar_kb/subscriptions/insertion_subscriber.py
+++ triplestar_kb/triplestar_kb/subscriptions/insertion_subscriber.py
@@ -1,4 +1,6 @@
 from jinja2 import Environment
+from rclpy.lifecycle import LifecycleNode
+from rclpy.node import Node
 from ros2topic.api import get_msg_class
 
 from triplestar_kb.subscriptions.query_time_subscriber import wait_for_topic
@@ -6,7 +8,7 @@ from triplestar_kb.subscriptions.query_time_subscriber import wait_for_topic
 
 class InsertionSubscriber:
     def __init__(self, node, config: dict, env: Environment, update_fn):
-        self._node = node
+        self._node: Node | LifecycleNode = node
         self._logger = node.get_logger().get_child('InsertionSubscriber')
         self._topic = config['topic']
         self._update_fn = update_fn
@@ -18,12 +20,19 @@ class InsertionSubscriber:
         if msg_type is None:
             raise RuntimeError(f'Unable to determine message class for {self._topic}')
 
-        self._template = env.get_template(config['template'])
+        try:
+            self._template = env.get_template(config['template'])
+        except Exception as e:
+            raise RuntimeError(f'Unable to load template {config["template"]}: {e}')
+
         self._sub = node.create_subscription(msg_type, self._topic, self._callback, 10)
         self._logger.info(f'Subscribed to {self._topic} with template {config["template"]}')
 
     def _callback(self, msg):
         try:
-            self._update_fn(self._template.render(msg=msg))
+            query = self._template.render(msg=msg)
+            self._update_fn(query)
+            self._logger.debug(f'Insertion succeeded: {query[:100]}...')
+
         except Exception as e:
             self._logger.error(f'Insertion failed for {self._topic}: {e}')
